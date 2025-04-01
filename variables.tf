d5d9624f15fe6c10c09e6017d3cf20cee859f2d6
Julianm <julianmensah16@gmail.com>
2025-04-01 22:38:33 +0000

    my second commit

--- variables.tf
+++ variables.tf
@@ -5,17 +5,6 @@ variable "region" {
 variable "instance_type" {
   default = "t2.micro"
 }
-
-variable "instance_ami" {
-  default = "ami-02f624c08a83ca16f"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
-
 variable "key_name" {
   default = "cba_keypair4"
 }
